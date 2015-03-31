b9b0d89bc608b1d7a15448503916000179074e88
Mike <mcfink@gmail.com>
2015-03-30 20:45:11 -0400

    starting point for routes assignment

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 angular.module('waitstaffApp', [])
 	.controller('tipController', function($scope){
-		var calculated_subtotal = 0;
-		var calculated_tip = 0;
-		var tax_rate = 0;
-		var meal_price = 0;
-		var tip_percentage = 0;
-		var total = 0;
-		var meal_count = 0;
-		var tip_total = 0;
-		var average_tip = 0;
+		$scope.meal_count = 0;
+		$scope.tip_total = 0;
+		$scope.average_tip = 0;
 
-		$scope.tax_rate = tax_rate;
-		$scope.meal_price = meal_price;
-		$scope.tip_percentage = tip_percentage;
-		$scope.calculated_subtotal = calculated_subtotal;
-		$scope.total = total;
+		$scope.tax_rate = 0;
+		$scope.meal_price = 0;
+		$scope.tip_percentage = 0;
+		$scope.subtotal = 0;
+		$scope.total = 0;
+		$scope.tip = 0;
 
 		$scope.submit = function(){
-			calculated_subtotal = $scope.meal_price * (1 + ($scope.tax_rate / 100));
-			calculated_tip = calculated_subtotal * $scope.tip_percentage / 100;
-			total = calculated_subtotal + calculated_tip;
+			$scope.subtotal = $scope.meal_price * (1 + ($scope.tax_rate / 100));
+			$scope.tip = $scope.subtotal * $scope.tip_percentage / 100;
+			$scope.total = $scope.subtotal + $scope.tip;
 
-			tip_total += calculated_tip;
-			meal_count++;
-			average_tip = (tip_total / meal_count);
-
-			$scope.subtotal = calculated_subtotal;
-			$scope.tip = calculated_tip;
-			$scope.total = total;
-			$scope.meal_count = meal_count;
-			$scope.tip_total = tip_total;
-			$scope.average_tip = average_tip;
-
+			$scope.tip_total += $scope.tip;
+			$scope.meal_count++;
+			$scope.average_tip = ($scope.tip_total / $scope.meal_count);
 		}
 
 		$scope.cancel = function(){
@@ -42,18 +29,13 @@ angular.module('waitstaffApp', [])
 
 		$scope.reset = function(){
 			this.cancel();
-			calculated_subtotal = 0;
-			calculated_tip = 0;
-			total = 0;
-			tip_total = 0;
-			meal_count = 0;
-			average_tip = 0;
 
-			$scope.subtotal = calculated_subtotal;
-			$scope.tip = calculated_tip;
-			$scope.total = total;
-			$scope.meal_count = meal_count;
-			$scope.tip_total = tip_total;
-			$scope.average_tip = average_tip;
+
+			$scope.subtotal = 0;
+			$scope.tip = 0;
+			$scope.total = 0;
+			$scope.meal_count = 0;
+			$scope.tip_total = 0;
+			$scope.average_tip = 0;
 		}
 	});
